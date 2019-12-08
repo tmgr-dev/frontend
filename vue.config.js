0fce9becfdd71adb89213c5e0428398391fdf318
Iurii Karpov <acrossoffwest@gmail.com>
2019-12-08 17:03:47 +0100

    (add) Components layouts: Index

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,10 @@
-module.exports = { runtimeCompiler: true, }
+module.exports = {
+    runtimeCompiler: true,
+    chainWebpack: config => {
+        config.module
+            .rule('images')
+            .use('url-loader')
+            .loader('url-loader')
+            .tap(options => Object.assign(options, { limit: 10240 }))
+    }
+}
