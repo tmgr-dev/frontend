844b46e8e73ccb0a167919af031e6202693abbff
savayer <sava0094@gmail.com>
2020-05-06 17:44:32 +0800

    (fix) updated gitignore, tabs and typos fixed

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,52 +1,52 @@
 import AuthBase from '../AuthBase'
 
 export default {
-  name: 'Login',
-  components: {
-      AuthBase
-  },
-  props: [],
-  data () {
-    return {
-        email: null,
-        password: null,
-        errors: {}
-    }
-  },
-  computed: {
-
-  },
-  mounted () {
-
-  },
-  methods: {
-    login () {
-        this.$axios.post('auth/login', {
-            email: this.email,
-            password: this.password
-        })
-            .then(({data}) => {
-                this.$store.commit('token', data)
-                this.setUser()
-            })
-            .catch((error) => {
-                if (!error || !error.response) {
-                    return
-                }
-                this.errors = error.response.data.errors
-            })
+    name: 'Login',
+    components: {
+        AuthBase
     },
-      setUser () {
-          this.$axios.defaults.headers = {
-              Authorization: `Bearer ${this.$store.getters.token.token}`,
-              'X-Requested-With': 'XMLHttpRequest'
-          }
-          this.$axios.get('user').then(({data}) => {
-              this.$store.commit('user', data)
-              this.$router.push({name: 'CurrentTasksList'})
-          })
-      }
-  }
+    props: [],
+    data() {
+        return {
+            email: null,
+            password: null,
+            errors: {}
+        }
+    },
+    computed: {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+        login() {
+            this.$axios.post('auth/login', {
+                email: this.email,
+                password: this.password
+            })
+                .then(({ data }) => {
+                    this.$store.commit('token', data)
+                    this.setUser()
+                })
+                .catch((error) => {
+                    if (!error || !error.response) {
+                        return
+                    }
+                    this.errors = error.response.data.errors
+                })
+        },
+        setUser() {
+            this.$axios.defaults.headers = {
+                Authorization: `Bearer ${this.$store.getters.token.token}`,
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+            this.$axios.get('user').then(({ data }) => {
+                this.$store.commit('user', data)
+                this.$router.push({ name: 'CurrentTasksList' })
+            })
+        }
+    }
 }
 
 
