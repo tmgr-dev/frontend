72b9efc7984145a34e16c588e739341773bc5627
savayer <sava0094@gmail.com>
2020-05-07 16:39:47 +0800

    (upd) created tooltips with errors. loader in register and login buttons. main scss file except css and added checkbox custom view

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -8,6 +8,7 @@ export default {
     props: [],
     data() {
         return {
+            showLoader: false,
             form: {
                 email: null,
                 password: null,
@@ -15,23 +16,20 @@ export default {
             errors: {}
         }
     },
-    computed: {
-
-    },
-    mounted() {
-
-    },
     methods: {
         async login() {
             const { ...loginData } = this.form
 
             try {
+                this.showLoader = true
                 const { data } = await this.$axios.post('auth/login', loginData)
+                this.showLoader = false
                 
                 this.$store.commit('token', data.data)
                 this.setUser()
 
             } catch (error) {
+                this.showLoader = false
                 if (error && error.response) {
                     this.errors = error.response.data.errors
                 }
