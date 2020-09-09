7aeb48082e4b781d226b1922f6aeab49fdb0a93b
Yurij Karpov <acrossoffwest@gmail.com>
2020-09-10 00:43:19 +0200

    (fix) Fixed breadcrumbs

--- src/components/UIElements/Breadcrumbs.vue
+++ src/components/UIElements/Breadcrumbs.vue
@@ -1,14 +1,15 @@
 <template>
 	<div class="card transition duration-300 ease-in-out hover:shadow-sm flex flex-col my-5">
 		<div class="card-body py-4">
-			<nav aria-label="breadcrumb">
-				<ol class="breadcrumb flex" v-if="current">
-					<li class="breadcrumb-item text-gray-600" v-for="(item, i) in items" :key="i">
-						<router-link :to="item.to" href="#" class="text-gray-600 hover:text-purple-700 mx-2">
+			<nav aria-label="breadcrumb" class="text-left">
+				<ol class="breadcrumb md:flex" v-if="current">
+					<li class="inline md:hidden">Breadcrumbs: </li>
+					<li class="breadcrumb-item  inline text-gray-600" v-for="(item, i) in items" :key="i">
+						<router-link :to="item.to" href="#" class="text-gray-600 hover:text-purple-700 mx-0 md:mx-1">
 							{{ item.label }}
 						</router-link>
 					</li>
-					<li class="breadcrumb-item active text-purple-700 hover:text-purple-700 mx-2" aria-current="page">
+					<li class="breadcrumb-item active inline text-purple-700 hover:text-purple-700 md:mx-1" aria-current="page">
 						{{ current }}
 					</li>
 				</ol>
@@ -45,12 +46,21 @@
 	}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 	.breadcrumb .breadcrumb-item {
 		position: relative;
 	}
 
 	.breadcrumb .breadcrumb-item:not(:last-child):after {
 		content: '/';
+
+		@media screen and (max-width: 768px) {
+			content: '/';
+		}
+	}
+	@media screen and (max-width: 768px) {
+		.breadcrumb .breadcrumb-item:first-child:before {
+			content: '->';
+		}
 	}
 </style>
