5abad7999d02b3d5cd31c870f99631487d4f8109
Iurii Karpov <acrossoffwest@gmail.com>
2021-03-01 19:58:31 +0100

    (fix) Fixed Stats.vue

--- src/components/Public/Stats.vue
+++ src/components/Public/Stats.vue
@@ -4,47 +4,42 @@
 	</teleport>
 	<BaseLayout>
 		<template #header>
-			Statistics
 		</template>
 		<template #body>
-			<p class="px-2 mb-5">Simple statistics of service</p>
 			<div :class="`w-full md:w-auto md:flex-grow md:flex md:items-center md:block block ${$color('textMain')}`">
-				<table>
+				<h1 class="text-4xl text-center">Statistics</h1>
+				<p class="px-2 mb-5 mx-auto text-center">Simple statistics of service</p>
+				<table class="mx-auto">
 					<tbody>
 						<tr>
-							<th class="text-xl">Users</th>
+							<th class="text-xl text-left">Users</th>
 							<td class="px-5 text-2xl">{{ stats.users }}</td>
 						</tr>
 						<tr>
-							<th class="text-2xl">Active users</th>
-							<td class="px-5 text-2xl"></td>
+							<th class="text-2xl text-left" colspan="2">Active users</th>
 						</tr>
 						<tr>
-							<th class="text-xl">Last 24 hours</th>
+							<th class="text-xl text-left">Last 24 hours</th>
 							<td class="px-5 text-2xl">{{ stats.active_in_1_day }}</td>
 						</tr>
 						<tr>
-							<th class="text-xl">Last week</th>
+							<th class="text-xl text-left">Last week</th>
 							<td class="px-5 text-2xl">{{ stats.active_in_1_day }}</td>
 						</tr>
 						<tr>
-							<th class="text-xl">Last month</th>
+							<th class="text-xl text-left">Last month</th>
 							<td class="px-5 text-2xl">{{ stats.active_in_1_month }}</td>
 						</tr>
 						<tr>
-							<th class="text-xl">Last year</th>
+							<th class="text-xl text-left">Last year</th>
 							<td class="px-5 text-2xl">{{ stats.active_in_1_year }}</td>
 						</tr>
 						<tr>
-							<th class="text-xl">Users</th>
-							<td class="px-5 text-2xl">{{ stats.users }}</td>
-						</tr>
-						<tr>
-							<th class="text-xl">Tasks</th>
+							<th class="text-xl text-left">Tasks</th>
 							<td class="px-5 text-2xl">{{ stats.tasks }}</td>
 						</tr>
 						<tr>
-							<th class="text-xl">Сounted time</th>
+							<th class="text-xl text-left">Сounted time</th>
 							<td class="px-5 text-2xl">
 								<p>
 									In hours: {{ stats.hours }}<br>
