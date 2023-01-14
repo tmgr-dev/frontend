96ad8ea30c6effaad62a86f581983f2ef95f73d7
savayer <sava0094@gmail.com>
2023-01-14 23:20:17 +0800

    fixed formatting

--- src/components/UIElements/InputField.vue
+++ src/components/UIElements/InputField.vue
@@ -11,6 +11,7 @@
 							v-model="val"
 						/>
 					</div>
+
 					<textarea
 						:class="`${forCheckpoint ? 'max-h-40 pt-2 min-h-8' : ''} appearance-none border rounded w-full py-2 px-3 ${extraClass || $color('input')} ${$color('defaultBorder_darkNoBorder')} leading-tight focus:outline-none focus:shadow-outline`"
 						v-else-if="type === 'textarea'"
@@ -19,6 +20,7 @@
 						:style="forCheckpoint ? 'margin-bottom: -8px;' : ''"
 						:placeholder="placeholder"
 					/>
+
 					<div
 						v-else-if="type === 'contenteditable'"
 					>
@@ -29,8 +31,9 @@
 							content-type="html"
 							theme="bubble"
 							:placeholder="placeholder"
-						></quill-editor>
+						/>
 					</div>
+
 					<input
 						v-else-if="type === 'time_in_seconds'"
 						:id="name"
@@ -40,6 +43,7 @@
 						:placeholder="placeholder"
 						v-model="val"
 					/>
+
 					<div
 						v-else-if="type === 'checkbox'"
 						class="b-switch-list mt-3"
@@ -51,11 +55,13 @@
 								<input type="checkbox" :name="name" v-model="val" @keydown:enter="$emit('keydown:enter', val)">
 								<span></span>
 							</label>
+
 							<div class="b-switch-list__text">
 								<div class="b-switch-list__title" :class="$color('settingsTextColor')">{{ placeholder }}</div>
 							</div>
 						</div>
 					</div>
+
 					<input
 						v-else
 						:id="name"
@@ -65,12 +71,14 @@
 						:placeholder="placeholder"
 						v-model="val"
 					/>
+
 					<transition name="fade-left">
 						<div v-if="errors" class="error" :class="{ 'tooltip': errorAsTooltip }">
 							{{ errors[0] }}
 						</div>
 					</transition>
 				</div>
+
 				<div v-else>
 					{{ modelValue }} (<a href="#" @click.prevent="showInput = true">edit</a>)
 				</div>
@@ -79,16 +87,16 @@
 	</div>
 </template>
 
-
 <script>
 	import getTimeInSeconds from './InputField/getTimeInSeconds'
 	import toHHMM from './InputField/toHHMM'
 	import ContentEditable from './ContentEditable'
 
-
 	export default {
 		name: "InputField",
-		components: {ContentEditable},
+		components: {
+			ContentEditable
+		},
 		props: {
 			modelValue: {
 				required: false,
@@ -163,6 +171,7 @@
 					if (this.type !== 'time_in_seconds') {
 						return this.modelValue
 					}
+
 					return this.getSecondsInTime(this.modelValue)
 				},
 				set(v) {
@@ -172,6 +181,7 @@
 					if (this.type !== 'time_in_seconds') {
 						return this.$emit('update:modelValue', v)
 					}
+
 					this.$emit('update:modelValue', this.getTimeInSeconds(v))
 				}
 			},
@@ -213,62 +223,62 @@
 </script>
 
 <style scoped lang="scss">
-input.with-errors {
-	border: red solid 1px;
-}
+	input.with-errors {
+		border: red solid 1px;
+	}
 
-.error {
-	font-size: .9em;
-	color: red;
+	.error {
+		font-size: .9em;
+		color: red;
 
-	&.tooltip {
-		position: absolute;
-		left: 108%;
-		white-space: nowrap;
-		background: #fff;
-		padding: 0.75rem;
-		min-width: 220px;
-		display: inline-block;
-		top: -6px;
-		border-radius: 7px;
-		box-shadow: 1px 1px 4px #888;
-		line-height: 1.1;
-
-		div {
-			margin-bottom: 5px;
-		}
-
-		&:before {
-			content: '';
-			border: 6px solid transparent;
-			border-right-color: #ffffff;
+		&.tooltip {
 			position: absolute;
-			left: -12px;
-			z-index: 2;
-			top: calc(50% - 5px);
-		}
+			left: 108%;
+			white-space: nowrap;
+			background: #fff;
+			padding: 0.75rem;
+			min-width: 220px;
+			display: inline-block;
+			top: -6px;
+			border-radius: 7px;
+			box-shadow: 1px 1px 4px #888;
+			line-height: 1.1;
 
-		&:after {
-			content: '';
-			border: 8px solid transparent;
-			border-right-color: #d0d0d0;
-			position: absolute;
-			top: calc(50% - 7px);
-			left: -16px;
+			div {
+				margin-bottom: 5px;
+			}
+
+			&:before {
+				content: '';
+				border: 6px solid transparent;
+				border-right-color: #ffffff;
+				position: absolute;
+				left: -12px;
+				z-index: 2;
+				top: calc(50% - 5px);
+			}
+
+			&:after {
+				content: '';
+				border: 8px solid transparent;
+				border-right-color: #d0d0d0;
+				position: absolute;
+				top: calc(50% - 7px);
+				left: -16px;
+			}
 		}
 	}
-}
 
-.input_wrapper {
-	position: relative;
-}
+	.input_wrapper {
+		position: relative;
+	}
 
-.fade-left-enter-active, .fade-left-leave-active {
-	transition: .5s;
-	transform: translateX(0);
-}
+	.fade-left-enter-active, .fade-left-leave-active {
+		transition: .5s;
+		transform: translateX(0);
+	}
 
-.fade-left-enter-from, .fade-left-leave-to {
-	transform: translateX(25px);
-}
+	.fade-left-enter-from, .fade-left-leave-to {
+		transform: translateX(25px);
+	}
 </style>
