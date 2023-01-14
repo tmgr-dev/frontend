c65a8bc699c38da1e72a1214fbe0b10d3146d908
savayer <sava0094@gmail.com>
2023-01-15 00:31:21 +0800

    fixed breadcrumbs styles

--- src/components/UIElements/Breadcrumbs.vue
+++ src/components/UIElements/Breadcrumbs.vue
@@ -12,10 +12,11 @@
 					<li class="inline md:hidden">
 						Breadcrumbs:
 					</li>
+
 					<li
 						v-for="(item, i) in items"
 						:key="i"
-						class="breadcrumb-item  inline text-gray-600"
+						class="breadcrumb-item inline text-gray-600"
 						@dragleave="item.payload.hoverClass = ''"
 						@drop="drop($event, item.payload)"
 						@dragenter.prevent="item.payload.hoverClass = 'bg-red-500'"
@@ -23,14 +24,14 @@
 					>
 						<router-link
 							:to="item.to"
-							class="text-gray-600 hover:text-purple-700 mx-0 md:mx-1"
+							class="text-gray-300 hover:text-gray-100 mx-0 md:mx-1"
 						>
 							{{ item.label }}
 						</router-link>
 					</li>
 					<li
 						aria-current="page"
-						class="breadcrumb-item active inline text-purple-700 hover:text-purple-700 md:mx-1"
+						class="breadcrumb-item active inline text-gray-300 font-bold md:mx-1"
 					>
 						{{ current }}
 					</li>
@@ -38,12 +39,11 @@
 
 				<ol
 					v-else
-					class="breadcrumb flex"
-					style="padding-left: 0;"
+					class="breadcrumb flex pl-0"
 				>
 					<li
 						aria-current="page"
-						class="breadcrumb-item active text-purple-700 hover:text-purple-700 mx-2"
+						class="breadcrumb-item active text-gray-300"
 					>
 						Categories
 					</li>
@@ -54,49 +54,49 @@
 </template>
 
 <script>
-export default {
-	name: 'Breadcrumbs',
-	props: {
-		items: {
-			required: true,
-			type: Array
-		},
-		title: {
-			required: false,
-			type: String,
-			default: 'Breadcrumbs'
-		},
-		current: {
-			required: false,
-			type: String,
-			default: 'Current page'
-		},
-		drop: {
-			type: Function,
-			required: false,
-			default: () => () => {
+	export default {
+		name: 'Breadcrumbs',
+		props: {
+			items: {
+				required: true,
+				type: Array
+			},
+			title: {
+				required: false,
+				type: String,
+				default: 'Breadcrumbs'
+			},
+			current: {
+				required: false,
+				type: String,
+				default: 'Current page'
+			},
+			drop: {
+				type: Function,
+				required: false,
+				default: () => () => {
+				}
 			}
 		}
-	}
-};
+	};
 </script>
 
 <style lang="scss" scoped>
-.breadcrumb .breadcrumb-item {
-	position: relative;
-}
+	.breadcrumb .breadcrumb-item {
+		position: relative;
+	}
 
-.breadcrumb .breadcrumb-item:not(:last-child):after {
-	content: '/';
+	.breadcrumb .breadcrumb-item:not(:last-child):after {
+		content: '/';
+
+		@media screen and (max-width: 768px) {
+			content: '/';
+		}
+	}
 
 	@media screen and (max-width: 768px) {
-		content: '/';
+		.breadcrumb .breadcrumb-item:first-child:before {
+			content: '->';
+		}
 	}
-}
-
-@media screen and (max-width: 768px) {
-	.breadcrumb .breadcrumb-item:first-child:before {
-		content: '->';
-	}
-}
 </style>
