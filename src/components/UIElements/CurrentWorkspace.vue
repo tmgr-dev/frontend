3b63fe55894ec4166a228194d291aa87ad546748
Yurij Karpov <acrossoffwest@gmail.com>
2023-01-27 23:46:53 +0100

    add new workspace

--- src/components/UIElements/CurrentWorkspace.vue
+++ src/components/UIElements/CurrentWorkspace.vue
@@ -8,6 +8,32 @@
 		selected
 		type="select"
 	></input-field>
+	<a href="#" @click.prevent="isShowCreateNewWorkspaceModal = true" class="py-2 inline-block relative">Add new workspace <span class="material-icons ml-2 mt-0 absolute inline-block">add_circle_outline</span></a>
+	<Transition name="bounce-right-fade">
+		<modal
+			v-if="isShowCreateNewWorkspaceModal"
+			modal-class="p-6 w-96"
+			close-on-bg-click
+			@close="isShowCreateNewWorkspaceModal = false"
+		>
+			<template #modal-body>
+				<input-field
+					v-model="newWorkspace.name"
+					:errors="errors.title"
+					type="text"
+					extra-class="mb-1 bg-white dark:bg-gray-800"
+					placeholder="New workspace name"
+				/>
+				<button
+					@click="createNewWorkspace()"
+					class="w-full bg-orange-500 mr-5 mt-5 hover:bg-orange-600 transition text-white font-bold py-2 px-4 rounded focus:outline-none sm:mb-0"
+					type="button"
+				>
+					Create
+				</button>
+			</template>
+		</modal>
+	</Transition>
 </template>
 
 <script>
@@ -24,7 +50,13 @@
 		emits: ['update:modelValue'],
 		data() {
 			return {
+				isShowCreateNewWorkspaceModal: false,
 				workspaces: [],
+				errors: {},
+				newWorkspace: {
+					name: '',
+					type: 'test'
+				}
 			};
 		},
 		computed: {
@@ -37,7 +69,20 @@
 				},
 			},
 		},
-		methods: {},
+		methods: {
+			async createNewWorkspace () {
+				try {
+					let {
+						data: { data: workspaces },
+					} = await this.$axios.post('/workspaces', this.newWorkspace)
+
+				} catch (e) {
+					if (e.response && e.response && e.response.data.errors) {
+						this.errors = e.response.data.errors;
+					}
+				}
+			},
+		},
 		async created() {
 			let {
 				data: { data: workspaces },
