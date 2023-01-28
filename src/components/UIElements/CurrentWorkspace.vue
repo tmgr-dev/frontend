1dfbd2b290643b6be31237b3b60f09611052a436
Yurij Karpov <acrossoffwest@gmail.com>
2023-01-28 11:30:12 +0100

    add invitation to workspace page functionality

--- src/components/UIElements/CurrentWorkspace.vue
+++ src/components/UIElements/CurrentWorkspace.vue
@@ -9,6 +9,8 @@
 		type="select"
 	></input-field>
 	<a href="#" @click.prevent="isShowCreateNewWorkspaceModal = true" class="py-2 inline-block relative">Add new workspace <span class="material-icons ml-2 mt-0 absolute inline-block">add_circle_outline</span></a>
+	<br>
+	<a href="#" @click.prevent="isShowCreateWorkspaceInvitationModal = true" class="py-2 inline-block relative">Create invitation to workspace <span class="material-icons ml-2 mt-0 absolute inline-block">add_circle_outline</span></a>
 	<Transition name="bounce-right-fade">
 		<modal
 			v-if="isShowCreateNewWorkspaceModal"
@@ -34,6 +36,43 @@
 			</template>
 		</modal>
 	</Transition>
+	<Transition name="bounce-right-fade">
+		<modal
+			v-if="isShowCreateWorkspaceInvitationModal"
+			:modal-class="`p-6 ${newWorkspaceInvitation.token ? 'w-full' : 'w-96'}`"
+			close-on-bg-click
+			@close="isShowCreateWorkspaceInvitationModal = false"
+		>
+			<template #modal-body>
+				<div v-if="!newWorkspaceInvitation.token">
+					<input-field
+						v-model="newWorkspaceInvitation.max_usage_times"
+						:errors="errors.max_usage_times"
+						type="number"
+						extra-class="mb-1 bg-white dark:bg-gray-800"
+						placeholder="Max usage times"
+					/>
+					<input-field
+						v-model="newWorkspaceInvitation.expired_at"
+						:errors="errors.expired_at"
+						type="datetime-local"
+						extra-class="mb-1 bg-white dark:bg-gray-800"
+						placeholder="Expired at"
+					/>
+					<button
+						@click="createNewWorkspaceInvitation()"
+						class="w-full bg-orange-500 mr-5 mt-5 hover:bg-orange-600 transition text-white font-bold py-2 px-4 rounded focus:outline-none sm:mb-0"
+						type="button"
+					>
+						Create
+					</button>
+				</div>
+				<div v-else>
+					<a :href="invitationLink">{{ invitationLink }}</a>
+				</div>
+			</template>
+		</modal>
+	</Transition>
 </template>
 
 <script>
@@ -51,11 +90,16 @@
 		data() {
 			return {
 				isShowCreateNewWorkspaceModal: false,
+				isShowCreateWorkspaceInvitationModal: false,
 				workspaces: [],
 				errors: {},
 				newWorkspace: {
 					name: '',
 					type: 'test'
+				},
+				newWorkspaceInvitation: {
+					max_usage_times: '',
+					expired_at: null
 				}
 			};
 		},
@@ -68,6 +112,9 @@
 					return this.$emit('update:modelValue', v);
 				},
 			},
+			invitationLink () {
+				return `${document.location.protocol}//${document.location.host}/workspaces/invitations/${this.newWorkspaceInvitation.token}`
+			}
 		},
 		methods: {
 			async createNewWorkspace () {
@@ -76,6 +123,18 @@
 						data: { data: workspaces },
 					} = await this.$axios.post('/workspaces', this.newWorkspace)
 
+				} catch (e) {
+					if (e.response && e.response && e.response.data.errors) {
+						this.errors = e.response.data.errors;
+					}
+				}
+			},
+			async createNewWorkspaceInvitation () {
+				try {
+					let {
+						data: { data: newWorkspaceInvitation },
+					} = await this.$axios.post(`/workspaces/${this.val}/invitations`, this.newWorkspaceInvitation)
+					this.newWorkspaceInvitation = newWorkspaceInvitation
 				} catch (e) {
 					if (e.response && e.response && e.response.data.errors) {
 						this.errors = e.response.data.errors;
