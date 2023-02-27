35eb24e58c4a18da6eba179592ee9203669d0a79
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2023-02-27 17:11:54 +0200

    task-42

--- src/components/tasks/TaskBoardCard.vue
+++ src/components/tasks/TaskBoardCard.vue
@@ -12,12 +12,28 @@
 			</a>
 
 			<!--	@todo create component and implement adding new users	here	-->
+			<div class="relative flex flex-row-reverse ml-5">
+
 			<div
-				class="flex h-6 w-6 shrink-0 cursor-default rounded-full border-green-400 bg-green-600"
+				class="flex h-6 w-6 shrink-0 cursor-default rounded-full border-green-400 bg-green-600 shadow shadow-gray-300"
+				v-if="task.assignees.length > 0"
+				v-for="(item, i) in task.assignees"
+				key="i"
+				:class="{ '-mr-1': i > 0 }"
 			>
-				<div class="m-auto font-sans text-base text-white">
-					{{ user.name.charAt(0).toUpperCase() }}
+				<div class="m-auto font-sans text-base text-white" >
+					{{item.name.charAt(0).toUpperCase()}}
 				</div>
+			</div>
+
+			<div
+				class="flex h-6 w-6 shrink-0 cursor-default rounded-full border-green-400 bg-green-600 shadow shadow-gray-300"
+				v-else
+			>
+			<div class="m-auto font-sans text-base text-white"  >
+				{{task.user.name.charAt(0).toUpperCase()}}
+			</div>
+			</div>
 			</div>
 		</div>
 
