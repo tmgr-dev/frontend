3b8bdb948811c1a149fea536043dbf7453bce329
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2023-02-27 19:05:03 +0200

    task-42

---
+++ src/components/general/AssigneeAvatar.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="m-auto font-sans text-base text-white" >
+		{{name.charAt(0).toUpperCase()}}
+	</div>
+
+</template>
+
+<script>
+export default {
+	name: "AssigneeAvatar",
+	props: {
+
+		name: name
+	},}
+</script>
+
+<style scoped></style>
