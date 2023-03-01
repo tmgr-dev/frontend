e579a8410c4051b9d857feffe632b0c6a9eaab84
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2023-03-01 20:02:03 +0200

    task-43

--- src/components/general/AssigneeAvatar.vue
+++ src/components/general/AssigneeAvatar.vue
@@ -1,6 +1,16 @@
 <template>
 	<div class="m-auto font-sans text-base text-white" >
-		{{name.charAt(0).toUpperCase()}}
+		{{assignee?.name.charAt(0).toUpperCase()}}
+	</div>
+	<div v-if="isActiveDeleteAsign"
+		class="invisible absolute -top-1.5 -right-1.5 flex h-4 w-4 cursor-pointer rounded-full bg-red-500 opacity-75 hover:opacity-100 group-hover:visible"
+	>
+					<span
+						class="material-icons m-auto text-xs text-white"
+						@click="deleteAssignee(assignee.id)"
+					>
+						close
+					</span>
 	</div>
 
 </template>
@@ -9,8 +19,19 @@
 export default {
 	name: "AssigneeAvatar",
 	props: {
-
-		name: name
+		assignee:{
+			type: Object,
+			required:true
+		},
+		isActiveDeleteAsign:{
+			type:Boolean,
+			required: true,
+		}
+	},
+	methods: {
+		deleteAssignee(id){
+			this.$emit('deleteAssign',id)
+		}
 	},}
 </script>
 
