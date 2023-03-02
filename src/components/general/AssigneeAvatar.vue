852088072d047d58a56d22a4bea5a85142580749
savayer <sava0094@gmail.com>
2023-03-02 11:06:43 +0800

    refactored AssigneeUsers

--- src/components/general/AssigneeAvatar.vue
+++ src/components/general/AssigneeAvatar.vue
@@ -1,38 +1,34 @@
 <template>
-	<div class="m-auto font-sans text-base text-white" >
-		{{assignee?.name.charAt(0).toUpperCase()}}
-	</div>
-	<div v-if="isActiveDeleteAsign"
-		class="invisible absolute -top-1.5 -right-1.5 flex h-4 w-4 cursor-pointer rounded-full bg-red-500 opacity-75 hover:opacity-100 group-hover:visible"
-	>
-					<span
-						class="material-icons m-auto text-xs text-white"
-						@click="deleteAssignee(assignee.id)"
-					>
-						close
-					</span>
+	<div class="m-auto font-sans text-base text-white">
+		{{ assignee?.name.charAt(0).toUpperCase() }}
 	</div>
 
+	<div
+		v-if="showRemovingAssignee"
+		class="invisible absolute -top-1.5 -right-1.5 flex h-4 w-4 cursor-pointer rounded-full bg-red-500 opacity-75 hover:opacity-100 group-hover:visible"
+	>
+		<span
+			class="material-icons m-auto text-xs text-white"
+			@click="$emit('deleteAssign', assignee.id)"
+		>
+			close
+		</span>
+	</div>
 </template>
 
 <script>
-export default {
-	name: "AssigneeAvatar",
-	props: {
-		assignee:{
-			type: Object,
-			required:true
+	export default {
+		name: 'AssigneeAvatar',
+		emits: ['deleteAssign'],
+		props: {
+			assignee: {
+				type: Object,
+				required: true,
+			},
+			showRemovingAssignee: {
+				type: Boolean,
+				default: true,
+			},
 		},
-		isActiveDeleteAsign:{
-			type:Boolean,
-			required: true,
-		}
-	},
-	methods: {
-		deleteAssignee(id){
-			this.$emit('deleteAssign',id)
-		}
-	},}
+	};
 </script>
-
-<style scoped></style>
