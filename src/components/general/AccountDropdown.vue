298820355938885916f08731ba9b6f94282e8b59
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2023-03-17 09:59:26 +0200

    TM-1: create section in dropdown

--- src/components/general/AccountDropdown.vue
+++ src/components/general/AccountDropdown.vue
@@ -17,41 +17,95 @@
 
 		<div
 			v-if="isOpenProfileDropdown"
-			class="absolute right-0 custom-top bg-white dark:bg-gray-900 py-2 shadow-lg z-50"
+			class="absolute right-0 custom-top bg-white dark:bg-gray-900 py-2 shadow-lg z-50 border rounded dark:border-gray-700 border-gray-200"
 			@mouseleave="isOpenProfileDropdown = false"
 		>
-			<ul class="w-40 py-2 px-4">
-				<li class="px-4 py-1 md:p-2 lg:px-4 hover:opacity-75">
-					<router-link class="block" to="/profile"> Profile </router-link>
-				</li>
-				<li class="px-4 py-1 md:p-2 lg:px-4 hover:opacity-75">
-					<router-link class="block" to="/settings"> Settings </router-link>
-				</li>
-				<li class="px-4 py-1 md:p-2 lg:px-4 hover:opacity-75">
-					<router-link class="block" to="/archive"> Archive </router-link>
-				</li>
-				<li class="px-4 py-1 md:p-2 lg:px-4 hover:opacity-75">
-					<a class="block" href="#" @click.prevent="logout"> Logout </a>
-				</li>
-			</ul>
+			<div class="flex">
+				<ul class="w-32 py-2 px-4 dark:border-gray-700 border-r-2">
+					<li class="px-4 py-1 md:p-2 lg:px-4 hover:opacity-75">
+						<router-link class="block" to="/profile"> Profile </router-link>
+					</li>
+					<li class="px-4 py-1 md:p-2 lg:px-4 hover:opacity-75">
+						<router-link class="block" to="/settings"> Settings </router-link>
+					</li>
+					<li class="px-4 py-1 md:p-2 lg:px-4 hover:opacity-75">
+						<router-link class="block" to="/archive"> Archive </router-link>
+					</li>
+					<li class="px-4 py-1 md:p-2 lg:px-4 hover:opacity-75">
+						<a class="block" href="#" @click.prevent="logout"> Logout </a>
+					</li>
+				</ul>
+				<div class="w-56 py-2 px-4">
+					<div>
+						<Select
+							:options="workspaces"
+							v-model="workspaceId"
+							label-key="name"
+							value-key="id"
+							@updateSettings="updateSettings"
+						/>
+						<!--						<button type="button" @click="updateSettings">save</button>-->
+					</div>
+					<ul class="mt-4">
+						<li class="py-1" v-for="{ name } in workspaceUsers">
+							{{ name }}
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
 	import { logout as logoutAction } from 'src/actions/tmgr/auth';
+	import {
+		getWorkspaceMembers,
+		getWorkspaces,
+	} from 'src/actions/tmgr/workspaces';
+	import {
+		getUser,
+		getUserSettings,
+		getUserSettingsV2,
+		updateUserSettings,
+		updateUserSettingsV2,
+	} from 'src/actions/tmgr/user';
+	import Select from 'src/components/general/Select.vue';
+	import Button from 'src/components/general/Button.vue';
 
 	export default {
 		name: 'AccountDropdown',
+		components: {
+			Button,
+			Select,
+		},
+		emits: ['updateSettings'],
 		data() {
 			return {
 				isOpenProfileDropdown: false,
+				workspaces: [],
+				user: {},
+				settings: [],
+				workspaceId: '',
+				workspaceUsers: [],
+				availableSettings: [],
 			};
 		},
+
 		computed: {
 			switchOn() {
 				return this.$store.getters.colorScheme === 'dark';
 			},
+			userSettings() {
+				return this.$store.getters.getUserSettings || {};
+			},
+		},
+		async mounted() {
+			this.user = await getUser();
+			await this.loadSettings();
+		},
+		async created() {
+			this.workspaces = await getWorkspaces();
 		},
 		methods: {
 			async logout() {
@@ -65,6 +119,23 @@
 				} catch (e) {
 					console.error(e);
 				}
+			},
+			async loadSettings() {
+				this.workspaceId = this.user.settings[0].value;
+				this.workspaceUsers = await getWorkspaceMembers(this.workspaceId);
+			},
+			async updateSettings() {
+				console.log(this.workspaceId);
+				const data = await getUserSettingsV2();
+				console.log('data1', data);
+				this.user.settings.find(async (item) => {
+					const newUserSettings = data.find(
+						(element) => element.id === item.id,
+					);
+					newUserSettings.value = this.workspaceId;
+					await updateUserSettingsV2([newUserSettings]);
+					this.showAlert();
+				});
 			},
 		},
 	};
