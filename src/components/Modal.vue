c496cce8284896e2b9344bc406da530bc2029c22
savayer <sava0094@gmail.com>
2023-04-06 18:27:25 +0800

    refactored modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -13,40 +13,31 @@
 	</div>
 </template>
 
-<script>
-	export default {
-		name: 'Modal',
-		emits: ['close'],
-		props: {
-			closeOnBgClick: {
-				type: Boolean,
-				required: false,
-				default: true,
-			},
-			modalClass: {
-				type: String,
-				required: false,
-			},
-		},
-		data() {
-			return {
-				initialUrl: location.href,
-			};
-		},
+<script lang="ts" setup>
+	import { onUnmounted, ref } from 'vue';
 
-		methods: {
-			close(e) {
-				if (this.closeOnBgClick) {
-					if (e.target.dataset.name === 'overlay') {
-						this.$emit('close');
-					}
-				}
-			},
-		},
-		unmounted() {
-			if (location.href !== this.initialUrl) {
-				history.pushState({}, '', this.initialUrl);
+	interface Props {
+		modalClass: string;
+		closeOnBgClick: boolean;
+	}
+
+	const props = defineProps<Props>();
+	const emit = defineEmits(['close']);
+	const initialUrl = ref(location.href);
+
+	onUnmounted(() => {
+		if (location.href !== initialUrl.value) {
+			history.pushState({}, '', initialUrl.value);
+		}
+	});
+
+	function close(e: MouseEvent) {
+		if (props.closeOnBgClick) {
+			const target = e.target as HTMLDivElement;
+
+			if (target.dataset.name === 'overlay') {
+				emit('close');
 			}
-		},
-	};
+		}
+	}
 </script>
