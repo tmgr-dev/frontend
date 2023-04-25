b35727b77bd9e293925c383345e54adc4350bbfe
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2023-04-25 11:57:39 +0300

    TMGR-11: reordering statuses

--- src/components/general/FiltersBoard.vue
+++ src/components/general/FiltersBoard.vue
@@ -1,6 +1,15 @@
 <template>
 	<div class="w-full pl-4">
-		<div class="w-full flex justify-end mr-20 rounded">
+		<div class="w-full flex items-center justify-between mr-20 rounded">
+			<div class="flex items-center">
+				<input
+					class="cursor-pointer w-4 h-4 focus:outline-none rounded-lg"
+					type="checkbox"
+					id="checkbox"
+					@change="$emit('handleUpdateDraggable', $event.target.checked)"
+				/>
+				<label class="ml-2 text-sm" for="checkbox">Reorder statuses</label>
+			</div>
 			<div class="flex">
 				<div class="w-48 py-2">
 					<Select
@@ -21,11 +30,12 @@
 	import { useStore } from 'vuex';
 	import { defineEmits } from 'vue';
 	import Select from 'src/components/general/Select.vue';
-	const emit = defineEmits(['update:chosenUser']);
+	const emit = defineEmits(['update:chosenUser', 'handleUpdateDraggable']);
 
 	const props = defineProps({
 		workspaceUsers: Array,
 		chosenUser: Object,
+		activeDraggable: Boolean,
 	});
 
 	const store = useStore();
