dd7fb1178936c57bdade5748d1073775fefab547
Iurii Karpov <y.karpov@dats.team>
2023-06-03 17:12:22 +0200

    add calls to backend

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -6,12 +6,50 @@
 
 		<template #body>
 			<form class="form-horizontal mx-auto w-3/4" @submit.prevent="login">
-				<div class="mt-4 flex flex-col">
+				<div class="flex flex-col">
 					<div :style="{ color: errors ? 'red' : 'initial' }">
 						{{ message }}
 					</div>
 				</div>
-
+				<div class="flex flex-col">
+					<div class="text-center">
+						Login with
+					</div>
+				</div>
+				<div class="mt-6 flex flex-col">
+					<Button
+						class="my-1 flex justify-around rounded bg-slate-200 py-2 px-4 text-sm font-semibold text-black hover:bg-slate-300"
+						@click="loginWithSocialite('apple')"
+					>
+						<div class="flex">
+							<AppleIcon class="h-5 w-5" />
+							<span class="relative ml-3"> Apple </span>
+						</div>
+					</Button>
+					<Button
+						class="group my-1 flex justify-around rounded bg-black py-2 px-4 text-sm font-semibold text-white hover:bg-gray-800"
+						@click="loginWithSocialite('github')"
+					>
+						<div class="flex">
+							<GitHubIcon class="h-5 w-5" />
+							<span class="relative ml-3"> GitHub </span>
+						</div>
+					</Button>
+					<Button
+						class="my-1 flex justify-around rounded bg-red-400 py-2 px-4 text-sm font-semibold text-white hover:bg-red-700"
+						@click="loginWithSocialite('google')"
+					>
+						<div class="flex">
+							<GoogleIcon class="h-5 w-5" />
+							<span class="relative ml-3"> Google </span>
+						</div>
+					</Button>
+				</div>
+				<div class="mt-4 flex flex-col">
+					<div class="text-center">
+						or with
+					</div>
+				</div>
 				<div class="mt-4 flex flex-col">
 					<TextField
 						v-model="form.email"
@@ -33,7 +71,6 @@
 						type="password"
 					/>
 				</div>
-
 				<div class="mt-6 flex flex-col">
 					<Button
 						class="my-1 rounded bg-blue-500 py-2 px-4 text-sm font-semibold text-white hover:bg-blue-700"
@@ -43,24 +80,6 @@
 							Login
 							<loader v-if="isLoading" class="auth-loader" is-mini />
 						</span>
-					</Button>
-					<Button
-						class="my-1 flex justify-around rounded bg-slate-200 py-2 px-4 text-sm font-semibold text-black hover:bg-slate-300"
-					>
-						<AppleIcon class="h-5 w-5" />
-						<span class="relative"> Login with Apple </span>
-					</Button>
-					<Button
-						class="group my-1 flex justify-around rounded bg-black py-2 px-4 text-sm font-semibold text-white hover:bg-gray-800"
-					>
-						<GitHubIcon class="h-5 w-5" />
-						<span class="relative"> Login with GitHub </span>
-					</Button>
-					<Button
-						class="my-1 flex justify-around rounded bg-red-400 py-2 px-4 text-sm font-semibold text-white hover:bg-red-700"
-					>
-						<GoogleIcon class="h-5 w-5" />
-						<span class="relative"> Login with Google </span>
 					</Button>
 				</div>
 			</form>
@@ -88,7 +107,7 @@
 	import { useRouter } from 'vue-router';
 	import { ref } from 'vue';
 	import store from 'src/store';
-	import { Login, login as loginAction } from 'src/actions/tmgr/auth';
+	import { LoginRequest, login as loginAction } from 'src/actions/tmgr/auth';
 	import { getUser, getUserSettings } from 'src/actions/tmgr/user';
 	import { AxiosError } from 'axios';
 	import AuthBase from 'src/components/layouts/AuthBase.vue';
@@ -104,9 +123,10 @@
 	const form = ref({
 		email: '',
 		password: '',
-	} as Login);
+	} as LoginRequest);
 
-	// @todo for savayer only. Find out how to make it global
+	// TODO: for savayer only. Find out how to make it global
+	// "@todo" это откуда @savayer?
 	const isLoading = ref(false);
 	const message = ref('');
 	const errors = ref({});
@@ -133,5 +153,9 @@
 		} finally {
 			isLoading.value = false;
 		}
+	}
+
+	async function loginWithSocialite(platform: string) {
+		document.location.href = `${process.env.VUE_APP_API_BASE_URL}auth/login/${platform}`;
 	}
 </script>
