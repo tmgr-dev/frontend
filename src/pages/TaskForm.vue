d7b89468b50cddc2a77b12735a5ebaa3d3ba7aec
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2023-08-25 13:00:15 +0300

    tm-90:subtitle on change

--- src/pages/TaskForm.vue
+++ src/pages/TaskForm.vue
@@ -323,9 +323,9 @@
 						theme="bubble"
 						placeholder="Description"
 					/>
-					<div v-if="form.description?.length" class="w-full block my-2">
+					<div v-if="form.description?.length" class="my-2 block w-full">
 						<div
-							class="float-right opacity-50 hover:opacity-100 cursor-pointer"
+							class="float-right cursor-pointer opacity-50 hover:opacity-100"
 						>
 							<svg
 								v-if="isDescriptionOptimizing"
@@ -351,8 +351,16 @@
 							<svg
 								@click="optimizeWithAI"
 								v-else
-							 	xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-								<path fill-rule="evenodd" d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z" clip-rule="evenodd" />
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="h-6 w-6"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z"
+									clip-rule="evenodd"
+								/>
 							</svg>
 						</div>
 					</div>
@@ -443,7 +451,7 @@
 		addTaskAssignee,
 		deleteTaskAssignee,
 		getTasksIndexes,
-		optimizeWithAI
+		optimizeWithAI,
 	} from 'src/actions/tmgr/tasks';
 	import {
 		getTaskSettings,
@@ -462,7 +470,6 @@
 	import CommentsChat from 'src/components/general/CommentsChat.vue';
 	import store from 'src/store';
 	import Checkpoints from 'src/components/general/Checkpoints.vue';
-
 
 	export default {
 		name: 'TaskForm',
@@ -649,9 +656,14 @@
 			},
 		},
 		methods: {
-			async optimizeWithAI () {
+			async optimizeWithAI() {
 				this.isDescriptionOptimizing = true;
-				const result = await optimizeWithAI(`${this.form.title}: ${this.form.description.replace(/(<([^>]+)>)/gi, "")}`);
+				const result = await optimizeWithAI(
+					`${this.form.title}: ${this.form.description.replace(
+						/(<([^>]+)>)/gi,
+						'',
+					)}`,
+				);
 				this.form.description = result;
 
 				this.isDescriptionOptimizing = false;
@@ -858,7 +870,6 @@
 						return;
 
 					for (const setting of this.currentCategory.settings) {
-						console.log('settingkey', setting.key);
 						if (setting.key === 'task_name_pattern_date&time') {
 							const indexes = await getTasksIndexes(this.currentCategory.id);
 							for (const setting of this.currentCategory.settings) {
@@ -866,12 +877,23 @@
 									const indexes = await getTasksIndexes(
 										this.currentCategory.id,
 									);
-									this.form.title = titlePatternHandler(
+									const category = titlePatternHandler(
 										setting.value,
 										new Map(Object.entries(indexes)),
 									);
+									if (this.form.title) {
+										if (this.form.title.includes(':')) {
+											const cuttedTitle = this.form.title.split(':')[1].trim();
+											this.form.title = `${category} ${cuttedTitle}`;
+										} else {
+											this.form.title = `${category} ${this.form.title}`;
+										}
+									} else {
+										this.form.title = category;
+									}
 								}
 							}
+							x;
 							if (setting.key === 'approximately_time') {
 								this.form.approximately_time = parseInt(setting.value);
 							}
