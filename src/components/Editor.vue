b95257ebcd989c580dd6e17f5eb3e110d1a9e71b
savayer <sava0094@gmail.com>
2024-07-24 20:08:43 +0400

    added new editor, updated tw

---
+++ src/components/Editor.vue
@@ -0,0 +1,72 @@
+<template>
+	<MdEditor
+		ref="editorRef"
+		v-model="value"
+		:preview="false"
+		language="en-US"
+		placeholder="Add description..."
+		:theme="store.state.colorScheme === 'default' ? 'light' : 'dark'"
+		:toolbars-exclude="[
+			'sub',
+			'sup',
+			'mermaid',
+			'katex',
+			'revoke',
+			'next',
+			'save',
+			'prettier',
+			'fullscreen',
+			'preview',
+			'previewOnly',
+			'htmlPreview',
+			'catalog',
+			'github',
+		]"
+		:footers="['=', 0]"
+	>
+		<template #defFooters>
+			<button
+				@click="togglePreview"
+				type="button"
+				class="flex h-full items-center gap-1.5 bg-gray-200/60 px-2 transition hover:bg-gray-200/90"
+			>
+				<EyeSlashIcon
+					v-if="editorPreviewActive"
+					class="size-3.5 stroke-neutral-600"
+				/>
+				<EyeIcon v-else class="size-3.5 stroke-neutral-600" />
+				<span class="text-neutral-600">Preview</span>
+			</button>
+		</template>
+	</MdEditor>
+</template>
+
+<script setup lang="ts">
+	import { computed, ref } from 'vue';
+	import { MdEditor, ExposeParam } from 'md-editor-v3';
+	import 'md-editor-v3/lib/style.css';
+	import store from '../store';
+	import { EyeIcon, EyeSlashIcon } from '@heroicons/vue/24/outline';
+
+	interface Props {
+		modelValue: string;
+	}
+
+	const props = defineProps<Props>();
+	const emit = defineEmits(['update:modelValue']);
+	const editorRef = ref<ExposeParam>();
+	const editorPreviewActive = ref(false);
+	const value = computed({
+		get() {
+			return props.modelValue;
+		},
+		set(value) {
+			emit('update:modelValue', value);
+		},
+	});
+
+	const togglePreview = () => {
+		editorPreviewActive.value = !editorPreviewActive.value;
+		editorRef.value?.togglePreviewOnly();
+	};
+</script>
--- src/pages/NewForm.vue
+++ src/pages/NewForm.vue
@@ -2,231 +2,103 @@
 	<teleport to="title">{{ form.title }}&nbsp;</teleport>
 
 	<div
-		class="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50"
+		class="flex h-full flex-col gap-4 overflow-y-auto rounded-lg p-6 md:w-[700px]"
 	>
-		<div
-			class="min-w-[700px] overflow-hidden rounded-lg bg-white shadow-xl transition-all duration-300 ease-in-out"
-			:class="{ 'w-[1100px]': isChatOpen }"
-		>
-			<div class="flex">
-				<div class="w-[700px] p-6">
-					<div class="mb-4 flex items-center justify-between">
-						<div class="relative">
-							<select
-								class="rounded-full bg-transparent text-xs font-medium text-blue-800"
-							>
-								<option>In progress</option>
-							</select>
-						</div>
+		<div class="flex items-center justify-between">
+			<div class="relative">
+				<select
+					class="rounded-full bg-transparent text-xs font-medium text-blue-800"
+				>
+					<option>In progress</option>
+				</select>
+			</div>
 
-						<button
-							@click="
-								() => {
-									$emit('close');
-								}
-							"
-						>
-							<XMarkIcon class="h-5 w-5 fill-neutral-600 hover:fill-black" />
-						</button>
-					</div>
+			<button @click="$emit('close')">
+				<XMarkIcon class="h-5 w-5 fill-neutral-600 hover:fill-black" />
+			</button>
+		</div>
 
-					<div class="mb-4 flex items-center justify-between">
-						<h3 class="text-lg font-bold text-gray-900">Task name</h3>
-					</div>
+		<div class="flex items-center justify-between">
+			<TextField
+				v-model="form.title"
+				class="w-full"
+				input-class="w-full text-lg font-bold border-0 !px-0 !bg-transparent"
+				placeholder="Task name"
+			/>
+		</div>
 
-					<div class="grid grid-cols-3 items-center gap-4">
-						<div class="flex items-center gap-2">
-							<div class="flex rounded bg-[#F4CD48]">
-								<PlantIcon class="m-auto h-10 w-10" />
-							</div>
-
-							<div class="w-full">
-								<div class="text-xs text-neutral-400">Category</div>
-
-								<Select
-									:options="[
-										{ title: 'Category 1', value: 1 },
-										{ title: 'Category 2', value: 2 },
-									]"
-									label-key="title"
-									value-key="value"
-								/>
-							</div>
-						</div>
-
-						<div class="flex items-center gap-2">
-							<div class="flex rounded border border-neutral-200">
-								<ProfileIcon class="h-8 w-8" />
-							</div>
-
-							<div>
-								<div class="text-xs text-neutral-400">Assignees</div>
-								<div class="text-sm text-gray-600">savayer, +2</div>
-							</div>
-						</div>
-
-						<div class="flex items-center gap-2">
-							<div class="flex rounded border border-neutral-200">
-								<CalendarIcon class="h-8 w-8" />
-							</div>
-
-							<div>
-								<div class="text-xs text-neutral-400">Due date</div>
-								<div class="text-sm text-gray-600">05 Nov</div>
-							</div>
-						</div>
-					</div>
-
-					<div class="my-4 flex">
-						<div class="mr-2 flex w-2/6 items-start">
-							<svg
-								class="mr-2 h-5 w-5 text-gray-500"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							<label
-								class="block text-sm font-bold text-gray-700"
-								for="Priority"
-								>Priority</label
-							>
-						</div>
-						<div class="w-4/6">
-							<select
-								id="priority"
-								class="w-full rounded bg-gray-800 p-2 text-white"
-							>
-								<option>Choose</option>
-							</select>
-						</div>
-					</div>
-
-					<div class="mb-4 flex">
-						<div class="mr-2 flex w-2/6 items-start">
-							<svg
-								class="mr-2 h-5 w-5 text-gray-500"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-							<label
-								class="block text-sm font-bold text-gray-700"
-								for="descriptor"
-								>Description</label
-							>
-						</div>
-						<div class="w-4/6">
-							<textarea
-								id="description"
-								rows="3"
-								class="w-full rounded bg-gray-800 p-2 text-white"
-							>
-It is a long established fact that a reader will be distracted by the readable content. Fact that a reader will be distracted by</textarea
-							>
-						</div>
-					</div>
-
-					<div class="mb-4">
-						<label class="mb-2 block text-sm font-bold text-gray-700"
-							>Attachments</label
-						>
-						<div
-							class="cursor-pointer rounded-lg border-2 border-dashed border-gray-300 p-6 text-center"
-						>
-							<svg
-								class="mx-auto h-12 w-12 text-gray-400"
-								fill="none"
-								stroke="currentColor"
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-								></path>
-							</svg>
-							<span class="mt-2 block text-sm font-medium text-gray-900"
-								>Upload file</span
-							>
-						</div>
-					</div>
-
-					<button
-						class="w-full rounded bg-blue-500 p-2 text-white transition-colors hover:bg-blue-600"
-					>
-						Continue
-					</button>
+		<div class="grid items-center gap-4 md:grid-cols-3">
+			<div class="flex items-center gap-2">
+				<div class="flex rounded bg-[#F4CD48]">
+					<PlantIcon class="m-auto h-10 w-10" />
 				</div>
 
-				<!-- Окно чата -->
-				<div
-					v-if="isChatOpen"
-					class="w-[300px] border-l border-gray-200 bg-white"
-				>
-					<div class="flex h-full flex-col p-4">
-						<h4 class="mb-2 text-lg font-bold">Chat</h4>
-						<div class="mb-2 flex-grow overflow-y-auto rounded border p-2">
-							<div
-								v-for="(message, index) in chatMessages"
-								:key="index"
-								class="mb-2"
-							>
-								<div class="font-bold">{{ message.sender }}</div>
-								<div>{{ message.text }}</div>
-							</div>
-						</div>
-						<div class="flex">
-							<input
-								v-model="newMessage"
-								@keyup.enter="sendMessage"
-								type="text"
-								placeholder="Type a message..."
-								class="flex-grow rounded-l border bg-gray-800 px-2 py-1 text-white"
-							/>
-							<button
-								@click="sendMessage"
-								class="rounded-r bg-blue-500 px-4 py-1 text-white hover:bg-blue-600"
-							>
-								Send
-							</button>
-						</div>
-					</div>
+				<div class="w-full">
+					<div class="text-xs text-neutral-400">Category</div>
+
+					<Select
+						:options="[
+							{ title: 'Category 1', value: 1 },
+							{ title: 'Category 2', value: 2 },
+						]"
+						label-key="title"
+						value-key="value"
+					/>
+				</div>
+			</div>
+
+			<div class="flex items-center gap-2">
+				<div class="flex rounded border border-neutral-200">
+					<ProfileIcon class="h-8 w-8" />
+				</div>
+
+				<div>
+					<div class="text-xs text-neutral-400">Assignees</div>
+					<div class="text-sm text-gray-600">savayer, +2</div>
+				</div>
+			</div>
+
+			<div class="flex items-center gap-2">
+				<div class="flex rounded border border-neutral-200">
+					<CalendarIcon class="h-8 w-8" />
+				</div>
+
+				<div>
+					<div class="text-xs text-neutral-400">Due date</div>
+					<div class="text-sm text-gray-600">05 Nov</div>
 				</div>
 			</div>
 		</div>
 
+		<Editor v-model="form.description" class="min-h-60 md:!h-72" />
+
 		<button
-			@click="toggleChat"
-			class="absolute top-1/2 right-4 -translate-y-1/2 transform rounded-full bg-blue-500 p-2 text-white transition-colors hover:bg-blue-600"
+			class="mt-auto w-full rounded bg-blue-500 p-2 text-white transition-colors hover:bg-blue-600"
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-6 w-6"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-				/>
-			</svg>
+			Continue
 		</button>
 	</div>
+
+	<!--
+	<button
+		@click="toggleChat"
+		class="absolute top-1/2 right-4 -translate-y-1/2 transform rounded-full bg-blue-500 p-2 text-white transition-colors hover:bg-blue-600"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+			/>
+		</svg>
+	</button>-->
 </template>
 
 <script setup lang="ts">
@@ -240,6 +112,8 @@ It is a long established fact that a reader will be distracted by the readable c
 	import Select from 'src/components/general/Select.vue';
 	import ProfileIcon from 'src/components/icons/ProfileIcon.vue';
 	import CalendarIcon from 'src/components/icons/CalendarIcon.vue';
+	import Editor from 'src/components/Editor.vue';
+	import TextField from 'src/components/general/TextField.vue';
 
 	interface Props {
 		isModal: boolean;
