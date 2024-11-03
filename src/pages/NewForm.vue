58f0fbb17c47dce68cc49643e17b6d1bae560aec
savayer <sava0094@gmail.com>
2024-11-03 20:00:21 +0400

    creating & removing tasks with the new form

--- src/pages/NewForm.vue
+++ src/pages/NewForm.vue
@@ -1,3 +1,159 @@
+<script setup lang="ts">
+	import {
+		XMarkIcon,
+		TrashIcon,
+		DocumentPlusIcon,
+		BookmarkIcon,
+	} from '@heroicons/vue/20/solid';
+	import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/outline';
+	import store from 'src/store';
+	import {
+		computed,
+		onBeforeMount,
+		onMounted,
+		reactive,
+		ref,
+		toRef,
+	} from 'vue';
+	import { useRoute, useRouter } from 'vue-router';
+	import {
+		createTask as createTaskAction,
+		deleteTask,
+		getTask,
+		stopTaskTimeCounter,
+		Task,
+	} from 'src/actions/tmgr/tasks';
+	import Editor from 'src/components/Editor.vue';
+	import TextField from 'src/components/general/TextField.vue';
+	import { getStatuses, Status } from 'src/actions/tmgr/statuses';
+	import 'vue-multiselect/dist/vue-multiselect.css';
+	import SettingsComponent from 'src/components/SettingsComponent.vue';
+	import {
+		Select,
+		SelectContent,
+		SelectItem,
+		SelectTrigger,
+		SelectValue,
+	} from 'src/components/ui/select';
+	import { Button } from 'src/components/ui/button';
+	import TimeCounter from 'src/components/TimeCounter.vue';
+	import {
+		getWorkspaceMembers,
+		WorkspaceMember,
+	} from 'src/actions/tmgr/workspaces';
+	import AssigneesCombobox from 'src/components/AssigneesCombobox.vue';
+	import { Category, getCategories } from 'src/actions/tmgr/categories';
+	import CategoriesCombobox from 'src/components/CategoriesCombobox.vue';
+
+	interface Props {
+		isModal: boolean;
+	}
+
+	const props = defineProps<Props>();
+	const emit = defineEmits(['close']);
+	const route = useRoute();
+	const router = useRouter();
+	// @todo find out why v-model doesn't work with reactive
+	//let form = reactive<Partial<Task>>({});
+	let form = ref<Partial<Task>>({
+		// @todo found out what the rudiment this is
+		status: 'created',
+		status_id: store.state.taskStatusId || undefined,
+	});
+	const modalTaskId = toRef(store.state, 'currentTaskIdForModal');
+	const modalProjectCategoryId = computed(
+		() => store.state.createTaskInProjectCategoryId,
+	);
+	const projectCategoryId = computed(() =>
+		form.value?.id
+			? null
+			: route.params.project_category_id || modalProjectCategoryId.value,
+	);
+	const taskId = computed(
+		() => modalTaskId.value || (route.params.id as string),
+	);
+	const statuses = ref<Status[]>();
+	const categories = ref<Category[]>([]);
+	const workspaceMembers = ref<WorkspaceMember[]>([]);
+
+	onBeforeMount(async () => {
+		const workspaceId = store.state.user?.settings?.find(
+			(setting: Record<string, string | number>) =>
+				setting.key === 'current_workspace',
+		)?.value;
+
+		try {
+			const [loadedStatuses, loadedCategories, loadedWorkspaceMembers] =
+				await Promise.all([
+					getStatuses(),
+					getCategories(),
+					workspaceId && getWorkspaceMembers(workspaceId),
+				]);
+			statuses.value = loadedStatuses;
+			categories.value = loadedCategories;
+			workspaceMembers.value = loadedWorkspaceMembers;
+		} catch (e) {
+			console.error(e);
+		}
+
+		if (taskId.value) {
+			if (props.isModal) {
+				history.pushState({}, '', `/${taskId.value}`);
+			}
+
+			form.value = await getTask(+taskId.value);
+			console.log(form.value);
+		}
+	});
+
+	onMounted(() => {
+		// window.onkeydown = this.getShortcutSaveListener();
+	});
+
+	const createTask = async () => {
+		console.log(form.value);
+		form.value = await createTaskAction(form.value as Task);
+
+		if (props.isModal) {
+			history.pushState({}, '', `/${form.value.id}`);
+			emit('close');
+			store.commit('incrementReloadTasksKey');
+		} else {
+			await router.push({
+				name: 'TasksEdit',
+				params: {
+					id: form.value.id,
+				},
+			});
+		}
+		/*if (!taskId) {
+		showAlert();
+	}*/
+	};
+
+	const removeTask = async () => {
+		if (taskId.value) {
+			if (form.value.start_time) {
+				form.value = await stopTaskTimeCounter(+taskId.value);
+			}
+
+			try {
+				await deleteTask(+taskId.value);
+
+				if (props.isModal) {
+					emit('close');
+					store.commit('incrementReloadTasksKey');
+				} else {
+					await router.replace('/');
+				}
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	};
+	const saveTask = () => {};
+</script>
+
 <template>
 	<teleport to="title">{{ form.title }}&nbsp;</teleport>
 
@@ -6,7 +162,7 @@
 		:class="[isModal ? 'md:w-[700px]' : 'container mx-auto pt-14']"
 	>
 		<header class="flex justify-between">
-			<Select v-model="selectedStatus">
+			<Select v-model="form.status_id">
 				<SelectTrigger class="w-40 border-0">
 					<SelectValue placeholder="status" />
 				</SelectTrigger>
@@ -14,7 +170,7 @@
 					<SelectItem
 						class="cursor-pointer text-gray-900 hover:bg-tmgr-light-blue hover:!text-white dark:text-gray-400"
 						v-for="status in statuses"
-						:value="status"
+						:value="status.id"
 						:show-check-mark="false"
 					>
 						<span
@@ -28,7 +184,7 @@
 
 			<SettingsComponent v-if="!isModal" :form="form" />
 
-			<button v-if="isModal" @click="$emit('close')">
+			<button v-if="isModal" @click="emit('close')">
 				<XMarkIcon
 					class="size-5 fill-neutral-600 hover:fill-black dark:hover:fill-white"
 				/>
@@ -41,8 +197,6 @@
 			input-class="w-full text-lg font-bold border-0 !px-0 !bg-transparent"
 			placeholder="Task name"
 		/>
-
-		<!--			<Countdown :init-task="form" :disabled="!form.id" />-->
 
 		<div class="grid grid-cols-2 gap-4 md:flex md:items-center">
 			<CategoriesCombobox
@@ -147,173 +301,4 @@
 			</div>
 		</footer>
 	</div>
-
-	<!--
-	<button
-		@click="toggleChat"
-		class="absolute top-1/2 right-4 -translate-y-1/2 transform rounded-full bg-blue-500 p-2 text-white transition-colors hover:bg-blue-600"
-	>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			class="h-6 w-6"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke="currentColor"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-			/>
-		</svg>
-	</button>-->
 </template>
-
-<script setup lang="ts">
-	import {
-		XMarkIcon,
-		TrashIcon,
-		DocumentPlusIcon,
-		BookmarkIcon,
-	} from '@heroicons/vue/20/solid';
-	import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/outline';
-	import store from 'src/store';
-	import { computed, onBeforeMount, reactive, ref, toRef } from 'vue';
-	import { useRoute } from 'vue-router';
-	import {
-		createTask as createTaskAction,
-		getTask,
-		Task,
-	} from 'src/actions/tmgr/tasks';
-	import Editor from 'src/components/Editor.vue';
-	import TextField from 'src/components/general/TextField.vue';
-	import { getStatuses, Status } from 'src/actions/tmgr/statuses';
-	import 'vue-multiselect/dist/vue-multiselect.css';
-	import SettingsComponent from 'src/components/SettingsComponent.vue';
-	import {
-		Select,
-		SelectContent,
-		SelectItem,
-		SelectTrigger,
-		SelectValue,
-	} from 'src/components/ui/select';
-	import { Button } from 'src/components/ui/button';
-	import TimeCounter from 'src/components/TimeCounter.vue';
-	import {
-		getWorkspaceMembers,
-		WorkspaceMember,
-	} from 'src/actions/tmgr/workspaces';
-	import AssigneesCombobox from 'src/components/AssigneesCombobox.vue';
-	import { Category, getCategories } from 'src/actions/tmgr/categories';
-	import CategoriesCombobox from 'src/components/CategoriesCombobox.vue';
-
-	interface Props {
-		isModal: boolean;
-	}
-
-	const props = defineProps<Props>();
-	const route = useRoute();
-	// @todo find out why v-model doesn't work with reactive
-	//let form = reactive<Partial<Task>>({});
-	let form = ref<Partial<Task>>({});
-	const modalTaskId = toRef(store.state, 'currentTaskIdForModal');
-	const statusId = computed(() => store.state.taskStatusId);
-	const modalProjectCategoryId = computed(
-		() => store.state.createTaskInProjectCategoryId,
-	);
-	const projectCategoryId = computed(() =>
-		form.value?.id
-			? null
-			: route.params.project_category_id || modalProjectCategoryId.value,
-	);
-	const taskId = computed(
-		() => modalTaskId.value || (route.params.id as string),
-	);
-	const statuses = ref<Status[]>();
-	const selectedStatus = ref<Status>();
-	const categories = ref<Category[]>([]);
-	const workspaceMembers = ref<WorkspaceMember[]>([]);
-
-	async function initComponent() {
-		const [loadedStatuses, loadedCategories] = await Promise.all([
-			getStatuses(),
-			getCategories(),
-		]);
-		statuses.value = loadedStatuses;
-		categories.value = loadedCategories;
-
-		if (taskId.value) {
-			if (props.isModal) {
-				history.pushState({}, '', `/${taskId.value}`);
-			}
-
-			form.value = await getTask(+taskId.value);
-			if (form.value.workspace_id) {
-				workspaceMembers.value = await getWorkspaceMembers(
-					form.value.workspace_id,
-				);
-			}
-			// this.workspaceMembers = await getWorkspaceMembers(this.form.workspace_id);
-			// window.onkeydown = this.getShortcutSaveListener();
-		}
-
-		/*if (this.projectCategoryId && this.isCreatingTask) {
-			this.form.project_category_id = this.projectCategoryId;
-		}
-		await this.loadCategory();
-		await this.loadTaskSettings();
-		this.$store.getters.getPusher
-			.private(`App.Workspace.${this.form.workspace_id}`)
-			.on('comment-added', ({ comment }) => {
-				if (this.taskId === comment.task_id) {
-					this.$refs.commentsChat.addingComment(comment);
-				}
-			})
-			.on('comment-updated', ({ comment }) => {
-				if (this.taskId === comment.task_id) {
-					this.$refs.commentsChat.editingComment(comment);
-				}
-			})
-			.on('comment-deleted', ({ comment }) => {
-				if (this.taskId === comment.task_id) {
-					this.$refs.commentsChat.deletingComment(comment);
-				}
-			});
-
-		this.$store.getters.getPusher
-			.private(`App.User.${this.$store.state.user.id}`)
-			.on('task-countdown-stopped', ({ task }) => {
-				const isCountdownStarted = !!this.form.start_time;
-				if (!isCountdownStarted) {
-					return;
-				}
-
-				this.setFormDataWithDelay(task);
-			})
-			.on('task-countdown-started', ({ task }) => {
-				const isCountdownStarted = !!this.form.start_time;
-				if (!isCountdownStarted) {
-					this.setFormDataWithDelay(task);
-				}
-			});*/
-	}
-
-	onBeforeMount(() => {
-		initComponent();
-	});
-
-	const createTask = async () => {
-		form.value.approximately_time = form.value.settings?.find(
-			(item: any) => item.key === 'approximately_time',
-		)?.value;
-		// @ts-ignore @todo fix types
-		form = await createTaskAction(form);
-		/*if (!taskId) {
-			showAlert();
-		}*/
-	};
-
-	const removeTask = () => {};
-	const saveTask = () => {};
-</script>
