9fe2d66e45d6580f865070f56ba2376c17568be3
savayer <sava0094@gmail.com>
2024-11-12 17:02:40 +0400

    removed checking pusher envs

--- src/store/plugins/pusher-beams-client.js
+++ src/store/plugins/pusher-beams-client.js
@@ -2,7 +2,7 @@ import * as PusherPushNotifications from '@pusher/push-notifications-web';
 
 let client = {};
 
-if ('PushManager' in window && import.meta.env.VITE_PUSHER_BEAMS_INSTANCE_ID) {
+if ('PushManager' in window) {
 	client = new PusherPushNotifications.Client({
 		instanceId: import.meta.env.VITE_PUSHER_BEAMS_INSTANCE_ID,
 	});
--- src/store/plugins/pusher.js
+++ src/store/plugins/pusher.js
@@ -4,19 +4,17 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 export default function (token) {
-	if (import.meta.env.VITE_PUSHER_KEY) {
-		return new Echo({
-			broadcaster: 'pusher',
-			key: import.meta.env.VITE_PUSHER_KEY,
-			cluster: import.meta.env.VITE_PUSHER_CLUSTER,
-			forceTLS: true,
-			authEndpoint: import.meta.env.VITE_API_BASE_URL + 'broadcasting/auth',
-			auth: {
-				headers: {
-					Accept: 'application/json',
-					Authorization: 'Bearer ' + (token ? token.token : null),
-				},
+	return new Echo({
+		broadcaster: 'pusher',
+		key: import.meta.env.VITE_PUSHER_KEY,
+		cluster: import.meta.env.VITE_PUSHER_CLUSTER,
+		forceTLS: true,
+		authEndpoint: import.meta.env.VITE_API_BASE_URL + 'broadcasting/auth',
+		auth: {
+			headers: {
+				Accept: 'application/json',
+				Authorization: 'Bearer ' + (token ? token.token : null),
 			},
-		});
-	}
+		},
+	});
 }
