37b9f9d3044f7df47b1f4b7a60b2b28bf6cd0abb
savayer <sava0094@gmail.com>
2024-11-13 23:00:40 +0400

    (wip) refactoring tasks list

--- src/pages/TasksListPage.vue
+++ src/pages/TasksListPage.vue
@@ -1,11 +1,138 @@
+<script setup>
+	import LoadingTasksList from '@/components/loaders/LoadingTasksList.vue';
+	import TasksListComponent from '@/components/tasks/TasksListComponent.vue';
+	import Confetti from '@/components/Confetti.vue';
+	import TaskForm from '@/pages/TaskForm.vue';
+	import { getTasks, getTasksByStatus } from '@/actions/tmgr/tasks';
+	import TextField from '@/components/general/TextField.vue';
+	import { getCategories } from '@/actions/tmgr/categories';
+	import Select from '@/components/general/Select.vue';
+	import { computed, onMounted, ref } from 'vue';
+	import { useRoute } from 'vue-router';
+
+	const route = useRoute();
+	const showCreateTaskForm = ref(false);
+	const selectableTasks = ref(false);
+	const errorLoading = ref(false);
+	const showSearchInput = ref(false);
+	const panel = ref(false);
+	const searchText = ref(null);
+	const searchTimeout = ref(null);
+	const summaryTimeString = ref(null);
+	const isLoading = ref(true);
+	const h1 = {
+		CurrentTasksList: 'Current tasks',
+		HiddenTasksList: 'Hidden tasks',
+		ArchiveTasksList: 'Archive tasks',
+	};
+	const tasks = ref([]);
+	const isLoadingActions = ref({});
+	const hasAbilityToShowConfetti = ref(false);
+	const categories = ref([]);
+	const selectedCategory = ref(null);
+	const showCategorySelect = ref(false);
+	const tasksListComponentRef = ref(null);
+
+	const status = computed(() => route.meta.status);
+
+	onMounted(async () => {
+		try {
+			const categoriesData = await getCategories();
+
+			categories.value = [
+				{ id: 0, title: 'All categories' },
+				...categoriesData.map((category) => ({
+					id: category.id,
+					title: category.title,
+				})),
+			];
+			const data = await loadTasks();
+			setLoadingActions(data);
+		} catch (e) {
+			console.error(e);
+		}
+	});
+
+	function setLoadingActions(tasks) {
+		tasks.forEach((task) => {
+			isLoadingActions.value[`hide-${task.id}`] = false;
+			isLoadingActions.value[`done-${task.id}`] = false;
+			isLoadingActions.value[`start-${task.id}`] = false;
+			isLoadingActions.value[`stop-${task.id}`] = false;
+			isLoadingActions.value[`activate-${task.id}`] = false;
+			isLoadingActions.value[`delete-${task.id}`] = false;
+		});
+	}
+
+	function findByCategory(id) {
+		if (!id) {
+			return;
+		} else {
+			const filteredTasks = tasks.value.filter(
+				(task) => task.category && task.category.id === id,
+			);
+			tasks.value = filteredTasks;
+		}
+	}
+
+	async function reloadTasks() {
+		hasAbilityToShowConfetti.value = true;
+		await loadTasks();
+	}
+
+	async function loadTasks() {
+		try {
+			isLoading.value = true;
+			clearTimeout(searchTimeout.value);
+
+			let fetchedTasks = [];
+
+			if (status.value) {
+				fetchedTasks = await getTasksByStatus(status.value, {
+					params: {
+						search: searchText.value,
+					},
+				});
+			} else {
+				fetchedTasks = await getTasks({
+					params: {
+						search: searchText.value,
+					},
+				});
+			}
+
+			summaryTimeString.value = formatTime(
+				fetchedTasks.reduce((summary, task) => task.common_time + summary, 0),
+			);
+			tasks.value = fetchedTasks;
+
+			return fetchedTasks;
+		} catch (e) {
+			console.error(e);
+			errorLoading.value = true;
+		} finally {
+			isLoading.value = false;
+		}
+	}
+
+	function formatTime(taskTime) {
+		let hours = Math.floor(taskTime / 3600);
+		let minutes = Math.ceil((taskTime % 3600) / 60);
+
+		return `${
+			hours > 0 ? hours + ' hour' + (hours > 1 ? 's' : '') : ''
+		} ${minutes} minute${minutes > 1 ? 's' : ''}`;
+	}
+</script>
+
 <template>
 	<teleport to="title">
-		{{ h1[$route.name] }}
+		{{ h1[route.name] }}
 	</teleport>
 
 	<BaseLayout>
 		<template #action>
-			<div class="items-center justify-between md:flex md:flex-nowrap">
+			<div class="items-center justify-between px-2 md:flex md:flex-nowrap">
 				<transition name="fade">
 					<div
 						v-if="summaryTimeString"
@@ -75,18 +202,18 @@
 						</span>
 					</a>
 
-					<a
-						href="#"
-						@click.prevent="selectAll()"
-						title="Select all"
-						class="pr-1"
-					>
-						<span
-							class="material-icons text-3xl text-gray-700 opacity-75 hover:opacity-100 sm:text-4xl"
+					<button type="button" @click="selectableTasks = !selectableTasks">
+						<svg
+							viewBox="0 0 1024 1024"
+							class="size-10"
+							:class="[selectableTasks ? 'fill-gray-400' : 'fill-gray-700']"
+							xmlns="http://www.w3.org/2000/svg"
 						>
-							done_all
-						</span>
-					</a>
+							<path
+								d="M598.186667 955.733333h-0.443734a17.117867 17.117867 0 0 1-15.240533-10.325333L476.859733 699.733333H426.666667a17.066667 17.066667 0 1 1 0-34.133333h35.5328l-136.533334-317.525333a17.066667 17.066667 0 0 1 22.408534-22.408534l317.525333 136.533334V426.666667a17.066667 17.066667 0 1 1 34.133333 0v50.158933l245.640534 104.8064a17.083733 17.083733 0 0 1 0.9216 30.976l-149.486934 74.752 153.924267 153.924267a17.0496 17.0496 0 0 1 0 24.132266l-85.333333 85.333334a17.0496 17.0496 0 0 1-24.132267 0l-153.856-153.856-73.9328 149.3504a17.066667 17.066667 0 0 1-15.291733 9.489066zM682.666667 750.933333a17.066667 17.066667 0 0 1 12.066133 5.000534L853.333333 914.5344 914.5344 853.333333l-158.600533-158.600533a17.015467 17.015467 0 0 1 4.437333-27.323733l137.6768-68.829867-222.037333-94.737067-0.1024-0.034133-301.9776-129.877333 129.860266 301.994666 0.034134 0.068267 95.453866 222.020267 68.096-137.5744A17.066667 17.066667 0 0 1 682.666667 750.933333z m-341.333334-51.2h-85.333333a17.066667 17.066667 0 1 1 0-34.133333h85.333333a17.066667 17.066667 0 1 1 0 34.133333z m-170.666666 0H85.333333a17.066667 17.066667 0 0 1-17.066666-17.066666v-85.333334a17.066667 17.066667 0 1 1 34.133333 0v68.266667h68.266667a17.066667 17.066667 0 1 1 0 34.133333zM85.333333 529.066667a17.066667 17.066667 0 0 1-17.066666-17.066667v-85.333333a17.066667 17.066667 0 0 1 34.133333 0v85.333333a17.066667 17.066667 0 0 1-17.066667 17.066667z m597.333334-170.666667a17.066667 17.066667 0 0 1-17.066667-17.066667v-85.333333a17.066667 17.066667 0 1 1 34.133333 0v85.333333a17.066667 17.066667 0 0 1-17.066666 17.066667zM85.333333 358.4a17.066667 17.066667 0 0 1-17.066666-17.066667v-85.333333a17.066667 17.066667 0 0 1 34.133333 0v85.333333a17.066667 17.066667 0 0 1-17.066667 17.066667z m597.333334-170.666667a17.066667 17.066667 0 0 1-17.066667-17.066666V102.4h-68.266667a17.066667 17.066667 0 1 1 0-34.133333h85.333334a17.066667 17.066667 0 0 1 17.066666 17.066666v85.333334a17.066667 17.066667 0 0 1-17.066666 17.066666zM85.333333 187.733333a17.066667 17.066667 0 0 1-17.066666-17.066666V85.333333a17.066667 17.066667 0 0 1 17.066666-17.066666h85.333334a17.066667 17.066667 0 0 1 0 34.133333H102.4v68.266667a17.066667 17.066667 0 0 1-17.066667 17.066666z m426.666667-85.333333h-85.333333a17.066667 17.066667 0 0 1 0-34.133333h85.333333a17.066667 17.066667 0 1 1 0 34.133333zM341.333333 102.4h-85.333333a17.066667 17.066667 0 0 1 0-34.133333h85.333333a17.066667 17.066667 0 0 1 0 34.133333z"
+							/>
+						</svg>
+					</button>
 
 					<a
 						href="#"
@@ -109,7 +236,7 @@
 				:tasks="tasks"
 				:status="status"
 				:is-loading-actions="isLoadingActions"
-				has-selectable
+				:has-selectable="selectableTasks"
 				@reload-tasks="reloadTasks"
 				ref="tasksListComponent"
 			/>
@@ -125,23 +252,18 @@
 			</div>
 
 			<loading-tasks-list v-if="isLoading" class="mx-2" />
-			<!--<loader v-if="showLoader" style="margin-top: 2rem" />-->
 		</template>
 	</BaseLayout>
 </template>
 
-<script>
-	import TasksListMixin from '@/mixins/TasksListMixin';
-	import LoadingButtonActions from '@/mixins/LoadingButtonActions';
+<!--<script>
 	import LoadingTasksList from '@/components/loaders/LoadingTasksList.vue';
-	import TasksListComponent from '@/components/tasks/TasksList.vue';
+	import TasksListComponent from '@/components/tasks/TasksListComponent.vue';
 	import Confetti from '@/components/Confetti.vue';
 	import TaskForm from '@/pages/TaskForm.vue';
 	import { getTasks, getTasksByStatus } from '@/actions/tmgr/tasks';
 	import TextField from '@/components/general/TextField.vue';
 	import { getCategories } from '@/actions/tmgr/categories';
-	import { hueFromHex } from '@/utils/convertColors';
-	import { computed } from 'vue';
 	import Select from '@/components/general/Select.vue';
 
 	export default {
@@ -154,7 +276,6 @@
 			TasksListComponent,
 			Select,
 		},
-		mixins: [LoadingButtonActions, TasksListMixin],
 		data: () => ({
 			showCreateTaskForm: false,
 			errorLoading: false,
@@ -244,7 +365,7 @@
 						});
 					}
 
-					this.summaryTimeString = this.getTaskFormattedTime(
+					this.summaryTimeString = this.formatTime(
 						tasks.reduce((summary, task) => task.common_time + summary, 0),
 					);
 					this.tasks = tasks;
@@ -263,10 +384,18 @@
 				}
 				this.$refs.tasksListComponent.selectAll();
 			},
+			formatTime(taskTime) {
+				let hours = Math.floor(taskTime / 3600);
+				let minutes = Math.ceil((taskTime % 3600) / 60);
+
+				return `${
+					hours > 0 ? hours + ' hour' + (hours > 1 ? 's' : '') : ''
+				}  ${minutes} minute${minutes > 1 ? 's' : ''}`;
+			},
 		},
 		async created() {
 			const data = await this.loadTasks();
 			this.setLoadingActions(data);
 		},
 	};
-</script>
+</script>-->
