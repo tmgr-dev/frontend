7e51ed4013241f5f08d0c9d4fb3cda3f4ed3ecb6
savayer <sava0094@gmail.com>
2024-11-16 19:39:48 +0400

    button filter styles

--- src/globalComponents.ts
+++ src/globalComponents.ts
@@ -1,6 +1,4 @@
 import BaseLayout from '@/components/layouts/BaseLayout.vue';
-import Button from '@/components/general/Button.vue';
-import NewButton from '@/components/general/NewButton.vue';
 import Loader from '@/components/loaders/Loader.vue';
 
-export default [BaseLayout, Button, NewButton, Loader];
+export default [BaseLayout, Loader];
