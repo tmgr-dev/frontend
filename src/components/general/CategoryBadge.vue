3ff29aa7dd988e7a60f40579c7c3545ccac95e0c
savayer <sava0094@gmail.com>
2024-11-16 17:08:32 +0400

    refactoring

--- src/components/general/CategoryBadge.vue
+++ src/components/general/CategoryBadge.vue
@@ -1,31 +1,3 @@
-<template>
-	<div v-if="props.category?.id" class="group flex items-center gap-1">
-		<router-link
-			:to="{
-				name: 'ProjectCategoryChildrenList',
-				params: { id: props.category.id },
-			}"
-			class="z-10 rounded bg-gray-700 p-1.5 text-xs !leading-none text-white lg:text-sm"
-		>
-			{{ props.category.title }}
-		</router-link>
-
-		<a
-			:href="`/project-categories/${props.category.id}/tasks/create`"
-			class="z-10 hidden opacity-10 hover:opacity-100 group-hover:block"
-			title="Create a task of this category"
-			@click.prevent="
-				store.commit('createTaskInProjectCategoryId', {
-					projectCategoryId: props.category.id,
-					statusId: props.statusId,
-				})
-			"
-		>
-			<PlusCircleIcon class="h-4 w-4 lg:h-6 lg:w-6" />
-		</a>
-	</div>
-</template>
-
 <script setup lang="ts">
 	import { Category } from '@/actions/tmgr/categories';
 	import store from '@/store';
@@ -38,3 +10,31 @@
 
 	const props = defineProps<Props>();
 </script>
+
+<template>
+	<div v-if="props.category?.id" class="group flex items-center gap-1">
+		<router-link
+			:to="{
+				name: 'ProjectCategoryChildrenList',
+				params: { id: props.category.id },
+			}"
+			class="z-10 rounded bg-gray-700 p-1.5 text-xs !leading-none text-white lg:text-sm"
+		>
+			{{ props.category.title }}
+		</router-link>
+
+		<button
+			@click.stop="
+				store.commit('createTaskInProjectCategoryId', {
+					projectCategoryId: props.category.id,
+					statusId: props.statusId,
+				})
+			"
+			type="button"
+			class="z-20 hidden opacity-10 hover:opacity-100 group-hover:block"
+			title="Create a task of this category"
+		>
+			<PlusCircleIcon class="h-4 w-4 lg:h-6 lg:w-6" />
+		</button>
+	</div>
+</template>
