bc1a71611a1f158e1cccb23fa2a347f6446cc472
Iurii Karpov <acrossoffwest@gmail.com>
2024-11-21 11:21:41 +0100

    Remove copyright

--- src/components/layouts/BaseLayout.vue
+++ src/components/layouts/BaseLayout.vue
@@ -16,18 +16,6 @@
 		</div>
 
 		<slot name="footer" />
-
-		<div
-			v-if="!noCopyright"
-			class="container mx-auto mb-5 mt-auto px-5 pb-5 text-center text-sm text-gray-600 md:text-base"
-		>
-			<p class="mt-4">
-				Task manager.
-				<a class="text-gray-500" href="#" target="_blank">Tmgr.dev</a>
-			</p>
-
-			<p>{{ copyright }}</p>
-		</div>
 	</div>
 </template>
 
