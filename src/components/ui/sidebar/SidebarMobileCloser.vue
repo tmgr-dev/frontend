7426426d977a742f6daca74caaecbfacf731c9b0
Iurii Karpov <y.karpov@dats.team>
2026-01-01 14:12:53 +0100

    Add sidebar closer after clicking on menu item on mobile app

---
+++ src/components/ui/sidebar/SidebarMobileCloser.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useSidebar } from './utils';
+import { useRouter } from 'vue-router';
+
+const { isMobile, setOpenMobile } = useSidebar();
+const router = useRouter();
+
+router.afterEach(() => {
+	if (isMobile.value) {
+		setOpenMobile(false);
+	}
+});
+</script>
+
+<template>
+	<slot />
+</template>
+
