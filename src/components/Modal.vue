a5f95d9934b4d573c095fe73ac5ab8a36cc9e830
Iurii Karpov <y.karpov@dats.team>
2026-01-30 00:36:10 +0100

    TM-155: Fix

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -14,7 +14,7 @@
 <script lang="ts" setup>
 	import { onMounted, onUnmounted, ref } from 'vue';
 	import { useStore } from 'vuex';
-import { useModalEscHandler } from '@/composable/useModalEscHandler.ts';
+import { useModalEscHandler } from '@/composable/useModalEscHandler';
 
 	interface Props {
 		modalClass?: string;
