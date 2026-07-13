d161f48aa5bfd87b8393349c3c48a8150a9c0a2c
acrossoffwest <1808737+acrossoffwest@users.noreply.github.com>
2026-07-13 20:40:20 +0200

    feat(theme): hide quick light/dark toggle when a palette is active

--- src/components/general/DarkMode.vue
+++ src/components/general/DarkMode.vue
@@ -3,18 +3,22 @@
 	import store from '@/store';
 	import { computed } from 'vue';
 
+	const isDefaultTheme = computed(
+		() => (store.state.theme || 'default') === 'default',
+	);
 	const switchOn = computed({
 		get() {
 			return store.state.colorScheme === 'dark';
 		},
 		set(value) {
+			if (!isDefaultTheme.value) return;
 			store.commit('setColorScheme', value ? 'dark' : 'default');
 		},
 	});
 </script>
 
 <template>
-	<a href="#" @click="switchOn = !switchOn">
+	<a v-if="isDefaultTheme" href="#" @click="switchOn = !switchOn">
 		<div v-if="switchOn" class="flex">
 			<Sun class="size-4" />
 			<span class="ml-2">Light mode</span>
